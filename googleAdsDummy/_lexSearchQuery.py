715547459cc4bbe0ba25af2b9eadc834716ccfb3
Menezess42 <arielmenezess@hotmail.com>
2025-12-30 13:33:03 -0300

    feat: I started developing a mini-compiler for using searchQuery. Using lex and parsers makes it easier to check for possible errors in searchQuery, whether in its structure or typos. It also facilitates the use of an AST to break down the API calls, resolving the AST nodes from the bottom up and combining all resource usage into a single return, since it's not accessing a real database but rather making calls to internal functions.

---
+++ googleAdsDummy/_lexSearchQuery.py
@@ -0,0 +1,29 @@
+keyWords = {
+    "SELECT": "SELECT",
+    "FROM": "FROM",
+    "WHERE": "WHERE",
+    "AND": "AND",
+    "BETWEEN": "BETWEEN",
+    "ORDER BY": "ORDER_BY",
+    "ASC": "ASC",
+    "LIMIT": "LIMIT",
+}
+
+ponctuation = {
+    ".": "DOT",
+    ",": "COMMA",
+    "=": "IQUAL",
+    ">": "MORE",
+    "<": "LESS",
+    "-": "DASH",
+    "'": "APOSTROPHE",
+}
+
+wordSeparation = {
+        " ": "WHITESPACE",
+        "\n": "NEWLINE"
+        }
+
+number_re = r"[0-9]"
+
+letter_re = r"[A-Za-z]"
