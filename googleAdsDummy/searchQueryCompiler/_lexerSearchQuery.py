0ad6ece537c85089ecb90b51c179d36d6ba22440
Menezess42 <arielmenezess@hotmail.com>
2026-01-03 22:14:06 -0300

    FET: advaancement in the lexer. Function to read digits(numbers)

--- googleAdsDummy/searchQueryCompiler/_lexerSearchQuery.py
+++ googleAdsDummy/searchQueryCompiler/_lexerSearchQuery.py
@@ -27,11 +27,13 @@ class TokenType(Enum):
 
     EOF = "EOF"
 
-class token:
+
+class Token:
     def __init__(self, type: TokenType, value: Any, position: int):
         self.type = type
         self.value = value
         self.position = position
+
 
 class Lexer:
     def __init__(self, text: str):
@@ -40,6 +42,53 @@ class Lexer:
         self.current_char = self.text[0] if text else None
 
         self.keywords = {
-                "SELECT": TokenType.SELECT,
-                "FROM": TokenType.FROM,....#HERE .....
-                }
+            "SELECT": TokenType.SELECT,
+            "FROM": TokenType.FROM,
+            "WHERE": TokenType.WHERE,
+            "AND": TokenType.AND,
+            "BETWEEN": TokenType.BETWEEN,
+            "ASC": TokenType.ASC,
+            "LIMIT": TokenType.LIMIT,
+        }
+
+        self.punctuation = {
+            ".": TokenType.DOT,
+            ",": TokenType.COMMA,
+            "=": TokenType.IQUAL,
+            ">": TokenType.MORE,
+            "<": TokenType.LESS,
+            "-": TokenType.DASH,
+            "'": TokenType.APOSTROPHE,
+        }
+
+    def advance(self):
+        self.pos += 1
+        if self.pos >= len(self.text):
+            self.current_char = None
+        else:
+            self.current_char = self.text[self.pos]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char in " \t\n\r":
+            self.advance()
+
+    def read_number(self):
+        start_pos = self.pos
+        num_str = ""
+
+        while self.current_char is not None and self.current_char.isdigit():
+            num_str += self.current_char
+            self.advance()
+
+        if self.current_char == ".":
+            num_str += self.current_char
+            self.advance()
+
+            while self.current_char is not None and self.current_char.isdigit():
+                num_str += self.current_char
+                self.advance()
+
+            return Token(TokenType.NUMBER, float(num_str), start_pos)
+
+    def read_string(self):
+        ...
