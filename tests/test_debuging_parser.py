bafee0654866f457b132b09f8aa56ac6212a6ef4
Menezess42 <arielmenezess@hotmail.com>
2026-01-12 13:55:18 -0300

    start to debug the 11 fails from test_parser

---
+++ tests/test_debuging_parser.py
@@ -0,0 +1,22 @@
+import pytest
+from googleAdsDummy.searchQueryCompiler.astNodes_SearchQuery import (
+    BetweenNode,
+    ComparisonNode,
+    QueryNode,
+)
+from googleAdsDummy.searchQueryCompiler.parser_SearchQuery import parse_query
+
+
+# First test to fail the unity test from the test_parser.py
+def test_where_and_limit(self):
+    """WHERE + LIMIT"""
+    query = """
+    SELECT campaign.id FROM campaign
+    WHERE campaign.status = 'ENABLED'
+    LIMIT 50
+    """
+    ast = parse_query(query)
+
+    assert ast.where is not None
+    assert ast.limit is not None
+    assert ast.order_by is None
