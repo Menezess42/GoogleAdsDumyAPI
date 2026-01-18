5aa87a52bd5227d2e66efc6a1e0d32293babeae4
Menezess42 <arielmenezess@hotmail.com>
2026-01-18 20:38:11 -0300

    feat: start to develop the World class that is responsible to deal with all the creation and freazing of the simulated ads world

---
+++ googleAdsDummy/engine/world.py
@@ -0,0 +1,15 @@
+
+
+class World:
+    def __init__(self):
+        ...
+
+    def __repr__(self):
+        ...
+
+    def _createCampaign(self):
+        ...
+
+    def listCampaign(self):
+        ...
+
