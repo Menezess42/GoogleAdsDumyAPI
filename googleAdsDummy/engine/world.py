c6cedeac6c6915f8ae21a9f8375134119733aae8
Menezess42 <arielmenezess@hotmail.com>
2026-01-21 18:59:18 -0300

    feat(world): define domain state and public structure

--- googleAdsDummy/engine/world.py
+++ googleAdsDummy/engine/world.py
@@ -1,10 +1,44 @@
+from pydantic import StrictFloat, StrictInt
+from googleAdsDummy.models.campaign import Campaign
+
+from googleAdsDummy.types import Anomaly_rules, Date_period, Profile_rules, WorldRulesSnapshot
 
 
 class World:
-    def __init__(self, seed: int, num_campaigns: int, datae_range: tuple(str="YYYY-MM-DD", str="YYYY-MM-DD"), weekend_factor: float, anomaly_rules: [enabled: bool,probability: float], allowed_profiles: list)-> None:
+    def __init__(
+        self,
+        seed: StrictInt | None,
+        num_campaigns: StrictInt,
+        weekend_factor: StrictFloat,
+        date_period: Date_period,
+        anomaly_rules: Anomaly_rules,
+        profile_rules: Profile_rules,
+    ) -> None:
+        self.seed = seed
+        self.num_campaigns = num_campaigns
+        self.weekend_factor = weekend_factor
+        self.date_period = date_period
+        self.anomaly_rules = anomaly_rules
+        self.profile_rules = profile_rules
+
+    def __repr__(self): ...  # Last thing  to be done
+
+    def _create_campaigns(self):
         ...
 
-    def __repr__(self):
-        ... # Last thing  to be done
+    def list_campaigns(self) -> list[Campaign]:
+        ...
+
+    def campaign_exists(self, id) -> bool:
+        ...
+
+    def get_campaign(self, id) -> Campaign:
+        ...
+
+    def is_date_valid(self, date) -> bool:
+        ...
+
+    def get_rules_snapshot(self) -> WorldRulesSnapshot:
+        ...
 
 
