25e13078a7f27b88469b6b1e4e8848a526d5aeff
Menezess42 <arielmenezess@hotmail.com>
2026-01-24 20:00:52 -0300

    just cleaning empty lines

--- tests/test_anomaly_rules.py
+++ tests/test_anomaly_rules.py
@@ -9,11 +9,9 @@ def test_accepts_list_and_tuple():
     assert a.probability == 0.5
     assert a.efects.spike_conversions == 0.5
 
-
 def test_probability_out_of_range():
     with pytest.raises(ValidationError):
         Anomaly_rules.model_validate([True, 1.5, (0.5, 0.2)])
-
 
 def test_effect_out_of_range():
     with pytest.raises(ValidationError):
--- tests/test_date_period.py
+++ tests/test_date_period.py
@@ -7,7 +7,6 @@ def test_accepts_tuple():
     d = Date_period.model_validate(("2024-01-01", "2025-01-01"))
     assert d.start_date < d.end_date
 
-
 def test_invalid_order():
     with pytest.raises(ValueError, match="start_date should be less than end_date"):
         Date_period.model_validate(("2025-01-01", "2024-01-01"))
