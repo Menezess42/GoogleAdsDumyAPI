69a7ca321760f0f5bd55d2c5e4e0949377132888
Menezess42 <arielmenezess@hotmail.com>
2026-01-26 07:37:31 -0300

    feat: add get_rules_snapshot function, responsible for returning WorldRulesSnapshot object that contains all the rules for that world

--- googleAdsDummy/engine/world.py
+++ googleAdsDummy/engine/world.py
@@ -1,8 +1,12 @@
-from typing import get_args
-from pprint import pprint
-from pydantic import StrictFloat, StrictInt
 from datetime import date
-from googleAdsDummy.engine.generators import generate_campaign, generate_campaign_profiles
+from typing import get_args
+
+from pydantic import StrictFloat, StrictInt
+
+from googleAdsDummy.engine.generators import (
+    generate_campaign,
+    generate_campaign_profiles,
+)
 from googleAdsDummy.models.campaign import Campaign
 from googleAdsDummy.models.profiles import ProfileType
 from googleAdsDummy.types import (
@@ -29,28 +33,27 @@ class World:
         self.date_period = date_period
         self.anomaly_rules = anomaly_rules
 
-
         self._allowed_profiles(profile_rules)
         self.profile_rules = profile_rules
 
         self._create_campaigns()
 
-
     def _allowed_profiles(self, profile_rules: Profile_rules) -> None:
         if not profile_rules.allow_profiles.issubset(set(get_args(ProfileType))):
             raise ValueError("allowed_profiles should have only existent profiles")
         if len(profile_rules.ensure_at_least_one) > self.num_campaigns:
-            raise ValueError("Number of profile ensurment can't be greateer that the number of campaigns")
+            raise ValueError(
+                "Number of profile ensurment can't be greateer that the number of campaigns"
+            )
 
     def _create_campaigns(self):
         campaigns = generate_campaign(self.seed, self.num_campaigns)
         self.campaigns = {obj.id: obj for obj in campaigns}
 
         campaigns_id = list(self.campaigns)
-        self.campaign_profiles = generate_campaign_profiles(self.seed, campaigns_id, self.profile_rules)
-
-        pprint(self.campaigns)
-        pprint(self.campaign_profiles)
+        self.campaign_profiles = generate_campaign_profiles(
+            self.seed, campaigns_id, self.profile_rules
+        )
 
     def list_campaigns(self) -> list[Campaign]:
         return list(self.campaigns.values())
@@ -67,15 +70,17 @@ class World:
     def is_date_valid(self, date_tobe_check: str) -> bool:
         date_tobe_check = date.fromisoformat(date_tobe_check)
 
-        return self.date_period.start_date <= date_tobe_check <= self.date_period.end_date
+        return (
+            self.date_period.start_date <= date_tobe_check <= self.date_period.end_date
+        )
 
-    def get_rules_snapshot(self) -> WorldRulesSnapshot:...
-        # return WorldRulesSnapshot(seed=self.seed,
-        #                           num_campaigns=self.num_campaigns,
-        #                           weekend_factor=self.weekend_factor,
-        #                           date_period=self.date_period,
-        #                           anomaly_rules=self.anomaly_rules,
-        #                           profile_rules=self.profile_rules,
-        #                           campaign_profiles=...)
-
-    def __repr__(self): ...  # Last thing  to be done
+    def get_rules_snapshot(self) -> WorldRulesSnapshot:
+        return WorldRulesSnapshot(
+            seed=self.seed,
+            num_campaigns=self.num_campaigns,
+            weekend_factor=self.weekend_factor,
+            date_period=self.date_period,
+            anomaly_rules=self.anomaly_rules,
+            profile_rules=self.profile_rules,
+            campaign_profiles=self.campaign_profiles,
+        )
