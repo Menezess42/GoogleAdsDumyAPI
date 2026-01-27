d265e2fb6f7d74148a1d892c636aa739b12bc25a
Menezess42 <arielmenezess@hotmail.com>
2026-01-27 15:58:03 -0300

    feat: dispatch dictionary for handlers for query's clause

---
+++ googleAdsDummy/query/handlers.py
@@ -0,0 +1,72 @@
+from datetime import date
+from typing import List, Union
+
+from googleAdsDummy.query.searchQuery_schema import SEARCH_QUERY_SCHEMA
+
+allow_resources = SEARCH_QUERY_SCHEMA["resources"]
+allow_operators = SEARCH_QUERY_SCHEMA["operators"]
+allow_clauses = SEARCH_QUERY_SCHEMA["clauses"]
+
+handlers_dictDispatch = {
+    "from_clause": lambda dictValue: handle_from(dictValue),
+    "where": lambda dictValue: handle_where(dictValue),
+    "select": lambda dictValue: handle_select(dictValue),
+}
+
+
+def handle_from(dictValue):
+    values = list(dictValue.values())
+    if not all(value in allow_resources for value in values):
+        raise ValueError(f"resouces {values} from FROM are not allowed")
+
+
+def handle_where(dictValue):
+    conditions = list(dictValue['conditions'])
+    print(conditions)
+    # Verify the fields
+        # verify the resources from the fields
+        # verify the identifiers from the fields
+    # Verify the operators
+    # verify the values in relation to the operator
+        # If operator between
+            # Verify if upper limit is greater than lower
+            # Verify if identifier is applicable to between operator
+        # If comparison
+            # Verify if identifier is aplicable to the operator
+
+
+def handle_select(dictValue):
+    ...
+
+
+def handle_identifiers(identifier: dict) -> List[str]:
+    resource = identifier["resource"]
+
+    if resource not in allow_resources:
+        raise ValueError(f"unexpected resource {resource} at WHERE condition")
+
+    field = identifier["field"]
+    allowed_fields = allow_resources[resource]["fields"]
+    if field not in allowed_fields:
+        raise ValueError(f"unexpected field {field} at WHERE condition")
+
+    return [resource, field, allowed_fields[field]["type"]]
+
+
+def check_lower_upper(
+    lower: Union[int, float, str], upper: Union[int, float, str]
+) -> List:
+    if type(lower) != type(upper):
+        raise ValueError(f"BETWEEN clause limit different types: {lower}, {upper}")
+
+    if isinstance(upper, (int, float)):
+        if upper <= lower:
+            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
+    else:
+        lower_date = date.fromisoformat(lower)
+        upper_date = date.fromisoformat(upper)
+        if upper_date <= lower_date:
+            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
+
+    return [lower, upper, type(lower).__name__]
+
