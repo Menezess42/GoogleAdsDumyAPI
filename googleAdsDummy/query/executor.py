c9076419eb8e2d2bb9782de095d06468c16ad227
Menezess42 <arielmenezess@hotmail.com>
2026-01-31 17:30:41 -0300

    test: minor manual testing

--- googleAdsDummy/query/executor.py
+++ googleAdsDummy/query/executor.py
@@ -2,20 +2,18 @@ from datetime import date
 from typing import List, Union
 
 from googleAdsDummy.engine.world import World
-from googleAdsDummy.query.handlers import handlers_dictDispatch
+from googleAdsDummy.query.handlers import dictDispatch_handlers
 from googleAdsDummy.query.searchQuery_schema import SEARCH_QUERY_SCHEMA
 
 
 class Executor:
     def __init__(self):
-        self.allow_resources = SEARCH_QUERY_SCHEMA["resources"]
-        self.allow_operators = SEARCH_QUERY_SCHEMA["operators"]
-        self.allow_clauses = SEARCH_QUERY_SCHEMA["clauses"]
+        ...
 
     def consultData(self, world: World, ast: dict) -> dict:
         self.world = world
         self.ast = ast
         for key, value in ast.items():
-            response = handlers_dictDispatch[key](value)
+            response = dictDispatch_handlers[key](value)
 
         return {}
