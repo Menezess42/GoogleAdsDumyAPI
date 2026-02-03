671f2d9a3b7abf6c2029de177fdb3706d34fbdb6
Menezess42 <arielmenezess@hotmail.com>
2026-02-03 15:12:33 -0300

    feat: verify WHERE clause
    finished the handler for the WHERE clause

--- googleAdsDummy/query/handlers.py
+++ googleAdsDummy/query/handlers.py
@@ -1,3 +1,4 @@
+import builtins
 from datetime import date
 from typing import List, Union
 
@@ -7,9 +8,11 @@ allow_resources = SEARCH_QUERY_SCHEMA["resources"]
 allow_comparison_operators = SEARCH_QUERY_SCHEMA["comparison_operators"]
 allow_between_operators = SEARCH_QUERY_SCHEMA["between_operators"]
 allow_clauses = SEARCH_QUERY_SCHEMA["clauses"]
-allow_fields = {key: value
-            for resources in SEARCH_QUERY_SCHEMA["resources"].values()
-            for key, value in resources["fields"].items()}
+allow_fields = {
+    key: value
+    for resources in SEARCH_QUERY_SCHEMA["resources"].values()
+    for key, value in resources["fields"].items()
+}
 
 
 dictDispatch_handlers = {
@@ -18,6 +21,7 @@ dictDispatch_handlers = {
     "select": lambda dictValue: handle_select(dictValue),
 }
 
+
 def handle_from(dictValue):
     values = list(dictValue.values())
     if not all(value in allow_resources for value in values):
@@ -25,29 +29,21 @@ def handle_from(dictValue):
 
 
 def handle_where(dictValue):
+    print(dictValue)
     conditions = list(dictValue["conditions"])
     for value in conditions:
-        verify_field(value['field'])
+        verify_field(value["field"])
         operator2Verify = list(value.items())[1:]
-        verify_operators(operator2Verify, value['field']['field'])
-        # Verify the operators
-        # verify the values in relation to the operator
-        # If operator between
-        # Verify if upper limit is greater than lower
-        # Verify if identifier is applicable to between operator
-        # If comparison
-        # Verify if identifier is aplicable to the operator
+        verify_operators(operator2Verify, value["field"]["field"])
+
 
 def verify_field(field):
     if field["resource"] not in allow_resources:
-        raise ValueError(
-            f"Unexpected column {field['resource']} in WHERE clause"
-        )
+        raise ValueError(f"Unexpected column {field['resource']} in WHERE clause")
     resource = field["resource"]
     if field["field"] not in allow_resources[resource]["fields"]:
-        raise ValueError(
-            f"Unexpected field {field['field']} in WHERE clause"
-        )
+        raise ValueError(f"Unexpected field {field['field']} in WHERE clause")
+
 
 def verify_operators(operators_list, field):
     lower_bound, upper_bound = operators_list
@@ -60,40 +56,54 @@ def verify_operators(operators_list, field):
             )
 
         response_loUp_chck = verify_lower_upper_limits(lower_bound[1], upper_bound[1])
-
-        if not limitsType_compatible_with_field((lower_bound[1], upper_bound[1]), field):
-            raise ValueError(
-                f"Field and limits are not the same type"
-            )
+        if not limitsType_compatible_with_field(response_loUp_chck, field):
+            raise ValueError(f"Field and limits are not the same type")
 
     elif lower_bound[1] in allow_comparison_operators:
-        # I operator not = I have to see if field int valid
-        if allow_fields[field]["type"] == "string" and lower_bound[1] != '=':
+        if allow_fields[field]["type"] == "string" and lower_bound[1] != "=":
             raise ValueError(
                 f"Field of type {allow_fields[field]['type']} doesn't work with the {lower_bound[1]} operator"
             )
 
     else:
+        raise ValueError(f"Unexpected operator in WHERE clause")
+
+
+def verify_lower_upper_limits(
+    lower: Union[int, float, str], upper: Union[int, float, str]
+) -> List:
+    if type(lower) != type(upper):
         raise ValueError(
-            f"Unexpected operator in WHERE clause"
-            )
+            f"Limits from BETWEEN clause has different types: {lower}, {upper}"
+        )
+
+    if isinstance(upper, (int, float)):
+        if upper <= lower:
+            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
+    else:
+        lower_date = date.fromisoformat(lower)
+        upper_date = date.fromisoformat(upper)
+        if upper_date <= lower_date:
+            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
+
+    return [lower, upper, type(lower).__name__]
 
 
 def limitsType_compatible_with_field(limits, field) -> bool:
-    lower, upper = limits
+    field_type = allow_fields[field]["type"]
 
-    field_type = allow_fields[field]["type"]  # ex: "int", "float", "date"
+    limitsType = builtins.__dict__[limits[-1]]
 
     type_compatibility = {
-        "int":   {int},
+        "int": {int},
         "float": {int, float},
-        "date":  {str},
+        "date": {str},
+        "str": {str},
     }
 
     allowed_types = type_compatibility.get(field_type, set())
 
-    return type(lower) in allowed_types and type(upper) in allowed_types
-
+    return limitsType in allowed_types
 
 
 def handle_select(dictValue): ...
@@ -113,35 +123,5 @@ def handle_identifiers(identifier: dict) -> List[str]:
     return [resource, field, allowed_fields[field]["type"]]
 
 
-def verify_lower_upper_limits(
-    lower: Union[int, float, str], upper: Union[int, float, str]
-) -> List:
-    if type(lower) != type(upper):
-        raise ValueError(f"BETWEEN clause limit different types: {lower}, {upper}")
-
-    if isinstance(upper, (int, float)):
-        if upper <= lower:
-            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
-    else:
-        lower_date = date.fromisoformat(lower)
-        upper_date = date.fromisoformat(upper)
-        if upper_date <= lower_date:
-            raise ValueError(f"BETWEEN clause invalid limit: <{lower}|{upper}>")
-
-    return [lower, upper, type(lower).__name__]
-
-
 if __name__ == "__main__":
-    verify_operators([('lower', '2024-01-01'), ('upper', '2025-01-02')], 'date')
-
-
-
-
-
-
-
-
-
-
-
-
+    verify_operators([("lower", "2024-01-01"), ("upper", "2025-01-02")], "date")
