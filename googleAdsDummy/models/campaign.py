400ab5adfc66a04e6cafca07f7f4622b7ffb42e0
Menezess42 <arielmenezess@hotmail.com>
2026-02-05 11:39:31 -0300

    feat: add gets for id and budget_amount

--- googleAdsDummy/models/campaign.py
+++ googleAdsDummy/models/campaign.py
@@ -5,3 +5,9 @@ class Campaign(BaseModel):
     id: str
     name: str
     budget_amount: float
+
+    def get_id(self):
+        return self.id
+
+    def get_budget_amount(self):
+        return self.budget_amount
